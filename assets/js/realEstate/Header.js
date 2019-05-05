a289ec0c5b0574e315889692f843afd0bd343861
leonredman <leonproart@yahoo.com>
2019-05-04 21:18:55 -0700

    Initial Commit

---
+++ assets/js/realEstate/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+	constructor() {
+		super();
+		this.state = {
+			name: 'Joe'
+		};
+	}
+	render() {
+		return (
+			<header>
+				<div> Logo </div>
+
+				<nav>
+					<a href="#">create ads</a>
+					<a href="#">About Us</a>
+					<a href="#">Log In</a>
+					<a href="#" className="register-btn">
+						Register
+					</a>
+				</nav>
+			</header>
+		);
+	}
+}
---
+++ public/js/dist/realEstate.js
@@ -0,0 +1,119 @@
+(function(modules) {
+    function webpackJsonpCallback(data) {
+        var chunkIds = data[0];
+        var moreModules = data[1];
+        var executeModules = data[2];
+        var moduleId, chunkId, i = 0, resolves = [];
+        for (;i < chunkIds.length; i++) {
+            chunkId = chunkIds[i];
+            if (installedChunks[chunkId]) resolves.push(installedChunks[chunkId][0]);
+            installedChunks[chunkId] = 0;
+        }
+        for (moduleId in moreModules) if (Object.prototype.hasOwnProperty.call(moreModules, moduleId)) modules[moduleId] = moreModules[moduleId];
+        if (parentJsonpFunction) parentJsonpFunction(data);
+        while (resolves.length) resolves.shift()();
+        deferredModules.push.apply(deferredModules, executeModules || []);
+        return checkDeferredModules();
+    }
+    function checkDeferredModules() {
+        var result;
+        for (var i = 0; i < deferredModules.length; i++) {
+            var deferredModule = deferredModules[i];
+            var fulfilled = true;
+            for (var j = 1; j < deferredModule.length; j++) {
+                var depId = deferredModule[j];
+                if (0 !== installedChunks[depId]) fulfilled = false;
+            }
+            if (fulfilled) {
+                deferredModules.splice(i--, 1);
+                result = __webpack_require__(__webpack_require__.s = deferredModule[0]);
+            }
+        }
+        return result;
+    }
+    var installedModules = {};
+    var installedChunks = {
+        realEstate: 0
+    };
+    var deferredModules = [];
+    function __webpack_require__(moduleId) {
+        if (installedModules[moduleId]) return installedModules[moduleId].exports;
+        var module = installedModules[moduleId] = {
+            i: moduleId,
+            l: false,
+            exports: {}
+        };
+        modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+        module.l = true;
+        return module.exports;
+    }
+    __webpack_require__.m = modules;
+    __webpack_require__.c = installedModules;
+    __webpack_require__.d = function(exports, name, getter) {
+        if (!__webpack_require__.o(exports, name)) Object.defineProperty(exports, name, {
+            enumerable: true,
+            get: getter
+        });
+    };
+    __webpack_require__.r = function(exports) {
+        if ("undefined" !== typeof Symbol && Symbol.toStringTag) Object.defineProperty(exports, Symbol.toStringTag, {
+            value: "Module"
+        });
+        Object.defineProperty(exports, "__esModule", {
+            value: true
+        });
+    };
+    __webpack_require__.t = function(value, mode) {
+        if (1 & mode) value = __webpack_require__(value);
+        if (8 & mode) return value;
+        if (4 & mode && "object" === typeof value && value && value.__esModule) return value;
+        var ns = Object.create(null);
+        __webpack_require__.r(ns);
+        Object.defineProperty(ns, "default", {
+            enumerable: true,
+            value: value
+        });
+        if (2 & mode && "string" != typeof value) for (var key in value) __webpack_require__.d(ns, key, function(key) {
+            return value[key];
+        }.bind(null, key));
+        return ns;
+    };
+    __webpack_require__.n = function(module) {
+        var getter = module && module.__esModule ? function() {
+            return module["default"];
+        } : function() {
+            return module;
+        };
+        __webpack_require__.d(getter, "a", getter);
+        return getter;
+    };
+    __webpack_require__.o = function(object, property) {
+        return Object.prototype.hasOwnProperty.call(object, property);
+    };
+    __webpack_require__.p = "";
+    var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
+    var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
+    jsonpArray.push = webpackJsonpCallback;
+    jsonpArray = jsonpArray.slice();
+    for (var i = 0; i < jsonpArray.length; i++) webpackJsonpCallback(jsonpArray[i]);
+    var parentJsonpFunction = oldJsonpFunction;
+    deferredModules.push([ "./assets/js/realEstate/realEstate.js", "vendors~realEstate" ]);
+    return checkDeferredModules();
+})({
+    "./assets/js/realEstate/Filter.js": function(module, __webpack_exports__, __webpack_require__) {
+        "use strict";
+        eval('/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Filter; });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n\nclass Filter extends react__WEBPACK_IMPORTED_MODULE_0__["Component"] {\n  constructor() {\n    super();\n    this.state = {\n      name: \'Joe\'\n    };\n  }\n\n  render() {\n    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {\n      id: "filter"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "inside"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "Filter"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("select", {\n      name: "neighbourhood",\n      className: "filters neighbourhood"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("option", null, "Ridgewood")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("select", {\n      name: "housetype",\n      className: "filters housetype"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("option", null, "Ranch")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("select", {\n      name: "bedrooms",\n      className: "filters bedrooms"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("option", null, "2 BR")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "filters price"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {\n      className: "title"\n    }, "Price"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {\n      type: "text",\n      name: "min-price",\n      className: "min-price"\n    }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {\n      type: "text",\n      name: "max-price",\n      className: "max-price"\n    })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "filters floor-space"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {\n      className: "title"\n    }, "Floor Space"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {\n      type: "text",\n      name: "min-floor-space",\n      className: "min-floor-space"\n    }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {\n      type: "text",\n      name: "max-floor-space",\n      className: "max-floor-space"\n    })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "filters extras"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {\n      className: "title"\n    }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("label", {\n      for: "extras"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, "Elevators"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {\n      name: "extras",\n      value: "elevator",\n      type: "checkbox"\n    })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("label", {\n      for: "extas"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, "Swimming Pool"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {\n      name: "extras",\n      value: "swimming-pool",\n      type: "checkbox"\n    })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("label", {\n      for: "extas"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, "Finished Basement"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {\n      name: "extras",\n      value: "finished basement",\n      type: "checkbox"\n    })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("label", {\n      for: "extas"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, "Gym"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {\n      name: "extras",\n      value: "gym",\n      type: "checkbox"\n    })))));\n  }\n\n}\n\n//# sourceURL=webpack:///./assets/js/realEstate/Filter.js?');
+    },
+    "./assets/js/realEstate/Header.js": function(module, __webpack_exports__, __webpack_require__) {
+        "use strict";
+        eval('/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Header; });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n\nclass Header extends react__WEBPACK_IMPORTED_MODULE_0__["Component"] {\n  constructor() {\n    super();\n    this.state = {\n      name: \'Joe\'\n    };\n  }\n\n  render() {\n    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("header", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, " Logo "), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("nav", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {\n      href: "#"\n    }, "create ads"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {\n      href: "#"\n    }, "About Us"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {\n      href: "#"\n    }, "Log In"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {\n      href: "#",\n      className: "register-btn"\n    }, "Register")));\n  }\n\n}\n\n//# sourceURL=webpack:///./assets/js/realEstate/Header.js?');
+    },
+    "./assets/js/realEstate/Listings.js": function(module, __webpack_exports__, __webpack_require__) {
+        "use strict";
+        eval('/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Header; });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n\nclass Header extends react__WEBPACK_IMPORTED_MODULE_0__["Component"] {\n  constructor() {\n    super();\n    this.state = {\n      name: \'Joe\'\n    };\n  }\n\n  render() {\n    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {\n      id: "listings"\n    }, "Listings>", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {\n      className: "search-area"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {\n      type: "text",\n      name: "search"\n    })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {\n      className: "sortby-area"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "results"\n    }, "390 results found"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "sort-options"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("select", {\n      name: "sortby",\n      className: "sortby"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("option", {\n      value: "price-asc"\n    }, "Highest Price"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("option", {\n      value: "price-asc"\n    }, "Lowest Price")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "view"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {\n      className: "fa fa-th-list",\n      "aria-hidden": "true"\n    }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {\n      className: "fa fa-th",\n      "aria-hidden": "true"\n    })))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {\n      className: "listings-results"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "listing"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "listing-img"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {\n      className: "Address"\n    }, "Address"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "details"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "user-img"\n    }, " "), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "user-details"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {\n      className: "user-name"\n    }, "Nina Smith"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {\n      className: "post-date"\n    }, "05/05/2017")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "listing-details"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "floor-space"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {\n      className: "fa fa-square-o",\n      "aria-hidden": "true"\n    })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, " 1000 ft\\xB2")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "bedrooms"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {\n      className: "fa fa-bed",\n      "aria-hidden": "true"\n    }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, "3 bedrooms")))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {\n      className: "bottom-info"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, "$1000 / month "), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {\n      className: "fa fa-map-maker",\n      "aria-hiddeb": "true"\n    }), " Ridgwood, NYv")))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {\n      className: "pagination"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", {\n      className: "pagination-nums"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, " Prev"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, " 1"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, " 2"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, " 3"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, " 4"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, " 5"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, " Next"))));\n  }\n\n}\n\n//# sourceURL=webpack:///./assets/js/realEstate/Listings.js?');
+    },
+    "./assets/js/realEstate/realEstate.js": function(module, __webpack_exports__, __webpack_require__) {
+        "use strict";
+        eval('__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _Header_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Header.js */ "./assets/js/realEstate/Header.js");\n/* harmony import */ var _Filter_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./Filter.js */ "./assets/js/realEstate/Filter.js");\n/* harmony import */ var _Listings_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./Listings.js */ "./assets/js/realEstate/Listings.js");\n\n\n\n\n\n\nclass App extends react__WEBPACK_IMPORTED_MODULE_0__["Component"] {\n  constructor() {\n    super();\n    this.state = {\n      name: \'Joe\'\n    };\n  }\n\n  render() {\n    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Header_js__WEBPACK_IMPORTED_MODULE_2__[/* default */ "a"], null), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {\n      id: "content-area"\n    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Filter_js__WEBPACK_IMPORTED_MODULE_3__[/* default */ "a"], null), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Listings_js__WEBPACK_IMPORTED_MODULE_4__[/* default */ "a"], null)));\n  }\n\n}\n\nconst app = document.getElementById(\'app\');\nreact_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render(react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(App, null), app);\n\n//# sourceURL=webpack:///./assets/js/realEstate/realEstate.js?');
+    }
+});
