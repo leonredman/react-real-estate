96141b5a2e97d1e5b6bf57dab6e647dd5d2be51d
leonredman <leonproart@yahoo.com>
2020-01-04 10:32:08 -0700

    project update

--- assets/js/realEstate/Listings.js
+++ assets/js/realEstate/Listings.js
@@ -6,6 +6,58 @@ export default class Header extends Component {
 		this.state = {
 			name: 'Leon'
 		};
+		this.loopListings = this.loopListings.bind(this);
+	}
+	loopListings() {
+		var { listingsData } = this.props;
+
+		return listingsData.map((listing, index) => {
+			return (
+				<div className="col-md-3" key={index}>
+					<div className="listing">
+						<div
+							className="listing-img"
+							style={{
+								background: `url("${listing.image}") no-repeat center center`
+							}}
+						>
+							<span className="address">{listing.address}</span>
+							<div className="details">
+								<div className="col-md-3">
+									<div className="user-img"> </div>
+								</div>
+
+								<div className="col-md-9">
+									<div className="user-details">
+										<span className="user-name">Nina Smith</span>
+										<span className="post-date">05/05/2017</span>
+									</div>
+									<div className="listing-details">
+										<div className="floor-space">
+											<i className="fa fa-square-o" aria-hidden="true" />
+											<span> 1000 ft&sup2;</span>
+										</div>
+										<div className="bedrooms">
+											<i className="fa fa-bed" aria-hidden="true" />
+											<span>{listing.bedrooms} bedrooms</span>
+										</div>
+									</div>
+
+									<div className="view-btn">view listing</div>
+								</div>
+							</div>
+						</div>
+						<div className="bottom-info">
+							<span className="price">${listing.price}</span>
+							<span className="location">
+								<i className="fa fa-map-marker" aria-hidden="true" />{' '}
+								{listing.city},{listing.state}
+							</span>
+						</div>
+					</div>
+				</div>
+			);
+		});
 	}
 	render() {
 		return (
@@ -28,167 +80,7 @@ export default class Header extends Component {
 					</div>
 				</section>
 
-				<section className="listings-results">
-					<div className="col-md-3">
-						<div className="listing">
-							<div className="listing-img">
-								<span className="address">23 Dexi Hills</span>
-								<div className="details">
-									<div className="col-md-3">
-										<div className="user-img"> </div>
-									</div>
-
-									<div className="col-md-9">
-										<div className="user-details">
-											<span className="user-name">Nina Smith</span>
-											<span className="post-date">05/05/2017</span>
-										</div>
-										<div className="listing-details">
-											<div className="floor-space">
-												<i className="fa fa-square-o" aria-hidden="true" />
-												<span> 1000 ft&sup2;</span>
-											</div>
-											<div className="bedrooms">
-												<i className="fa fa-bed" aria-hidden="true" />
-												<span>3 bedrooms</span>
-											</div>
-										</div>
-
-										<div className="view-btn">view listing</div>
-									</div>
-								</div>
-							</div>
-
-							<div className="bottom-info">
-								<span className="price">$1000 / month </span>
-								<span className="location">
-									<i className="fa fa-map-marker" aria-hidden="true" />{' '}
-									Ridgwood, NY
-								</span>
-							</div>
-						</div>
-					</div>
-
-					<div className="col-md-3">
-						<div className="listing">
-							<div className="listing-img">
-								<span className="address">23 Dexi Hills</span>
-								<div className="details">
-									<div className="col-md-3">
-										<div className="user-img"> </div>
-									</div>
-
-									<div className="col-md-9">
-										<div className="user-details">
-											<span className="user-name">Nina Smith</span>
-											<span className="post-date">05/05/2017</span>
-										</div>
-										<div className="listing-details">
-											<div className="floor-space">
-												<i className="fa fa-square-o" aria-hidden="true" />
-												<span> 1000 ft&sup2;</span>
-											</div>
-											<div className="bedrooms">
-												<i className="fa fa-bed" aria-hidden="true" />
-												<span>3 bedrooms</span>
-											</div>
-										</div>
-
-										<div className="view-btn">view listing</div>
-									</div>
-								</div>
-							</div>
-
-							<div className="bottom-info">
-								<span className="price">$1000 / month </span>
-								<span className="location">
-									<i className="fa fa-map-marker" aria-hidden="true" />{' '}
-									Ridgwood, NY
-								</span>
-							</div>
-						</div>
-					</div>
-
-					<div className="col-md-3">
-						<div className="listing">
-							<div className="listing-img">
-								<span className="address">23 Dexi Hills</span>
-								<div className="details">
-									<div className="col-md-3">
-										<div className="user-img"> </div>
-									</div>
-
-									<div className="col-md-9">
-										<div className="user-details">
-											<span className="user-name">Nina Smith</span>
-											<span className="post-date">05/05/2017</span>
-										</div>
-										<div className="listing-details">
-											<div className="floor-space">
-												<i className="fa fa-square-o" aria-hidden="true" />
-												<span> 1000 ft&sup2;</span>
-											</div>
-											<div className="bedrooms">
-												<i className="fa fa-bed" aria-hidden="true" />
-												<span>3 bedrooms</span>
-											</div>
-										</div>
-
-										<div className="view-btn">view listing</div>
-									</div>
-								</div>
-							</div>
-
-							<div className="bottom-info">
-								<span className="price">$1000 / month </span>
-								<span className="location">
-									<i className="fa fa-map-marker" aria-hidden="true" />{' '}
-									Ridgwood, NY
-								</span>
-							</div>
-						</div>
-					</div>
-
-					<div className="col-md-3">
-						<div className="listing">
-							<div className="listing-img">
-								<span className="address">23 Dexi Hills</span>
-								<div className="details">
-									<div className="col-md-3">
-										<div className="user-img"> </div>
-									</div>
-
-									<div className="col-md-9">
-										<div className="user-details">
-											<span className="user-name">Nina Smith</span>
-											<span className="post-date">05/05/2017</span>
-										</div>
-										<div className="listing-details">
-											<div className="floor-space">
-												<i className="fa fa-square-o" aria-hidden="true" />
-												<span> 1000 ft&sup2;</span>
-											</div>
-											<div className="bedrooms">
-												<i className="fa fa-bed" aria-hidden="true" />
-												<span>3 bedrooms</span>
-											</div>
-										</div>
-
-										<div className="view-btn">view listing</div>
-									</div>
-								</div>
-							</div>
-
-							<div className="bottom-info">
-								<span className="price">$1000 / month </span>
-								<span className="location">
-									<i className="fa fa-map-marker" aria-hidden="true" />{' '}
-									Ridgwood, NY
-								</span>
-							</div>
-						</div>
-					</div>
-				</section>
+				<section className="listings-results">{this.loopListings()}</section>
 
 				<section id="pagination">
 					<ul className="pages">
