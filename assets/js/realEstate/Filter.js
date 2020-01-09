aba8346af91409f380494ab737e858d2e60f40f1
leonredman <leonproart@yahoo.com>
2020-01-08 23:40:22 -0700

    project update

--- assets/js/realEstate/Filter.js
+++ assets/js/realEstate/Filter.js
@@ -12,20 +12,44 @@ export default class Filter extends Component {
 			<section id="filter">
 				<div className="inside">
 					<h4>Filter</h4>
-					<select name="neighbourhood" className="filters neighbourhood">
-						<option>Ridgewood</option>
+					<select
+						name="neighbourhood"
+						className="filters neighbourhood"
+						onChange={this.props.change}
+					>
+						<option value="Ridgewood">Ridgewood</option>
+						<option value="Miami">Miami</option>
 					</select>
-					<select name="housetype" className="filters housetype">
+					<select
+						name="housetype"
+						className="filters housetype"
+						onChange={this.props.change}
+					>
 						<option>Ranch</option>
 					</select>
-					<select name="bedrooms" className="filters bedrooms">
+					<input type="text" name="max-price" className="max-price" />
+					<select
+						name="bedrooms"
+						className="filters bedrooms"
+						onChange={this.props.change}
+					>
 						<option>2 BR</option>
 					</select>
 
 					<div className="filters price">
 						<span className="title">Price</span>
-						<input type="text" name="min-price" className="min-price" />
-						<input type="text" name="max-price" className="max-price" />
+						<input
+							type="text"
+							name="min-price"
+							className="min-price"
+							onChange={this.props.change}
+						/>
+						<input
+							type="text"
+							name="max-price"
+							className="max-price"
+							onChange={this.props.change}
+						/>
 					</div>
 					<div className="filters floor-space">
 						<span className="title">Floor Space</span>
@@ -33,30 +57,52 @@ export default class Filter extends Component {
 							type="text"
 							name="min-floor-space"
 							className="min-floor-space"
+							onChange={this.props.change}
 						/>
 						<input
 							type="text"
 							name="max-floor-space"
 							className="max-floor-space"
+							onChange={this.props.change}
 						/>
 					</div>
 					<div className="filters extras">
 						<span className="title" />
-						<label for="extras">
+						<label htmlFor="extras">
 							<span>Elevators</span>
-							<input name="extras" value="elevator" type="checkbox" />
+							<input
+								name="extras"
+								value="elevator"
+								type="checkbox"
+								onChange={this.props.change}
+							/>
 						</label>
-						<label for="extas">
+						<label htmlFor="extas">
 							<span>Swimming Pool</span>
-							<input name="extras" value="swimming-pool" type="checkbox" />
+							<input
+								name="extras"
+								value="swimming-pool"
+								type="checkbox"
+								onChange={this.props.change}
+							/>
 						</label>
-						<label for="extas">
+						<label htmlFor="extas">
 							<span>Finished Basement</span>
-							<input name="extras" value="finished basement" type="checkbox" />
+							<input
+								name="extras"
+								value="finished basement"
+								type="checkbox"
+								onChange={this.props.change}
+							/>
 						</label>
-						<label for="extas">
+						<label htmlFor="extas">
 							<span>Gym</span>
-							<input name="extras" value="gym" type="checkbox" />
+							<input
+								name="extras"
+								value="gym"
+								type="checkbox"
+								onChange={this.props.change}
+							/>
 						</label>
 					</div>
 				</div>
