217aa4ed56fe0d06452db89508e28adfe1495bc9
leonredman <leonproart@yahoo.com>
2021-01-06 23:32:03 -0700

    project update css

---
+++ Listings.js
@@ -0,0 +1,20 @@
+import React, { Component} from 'react'
+import ReactDOM from 'react-dom'
+
+export default class Listings extends Component {
+  constructor () {
+    super()
+    this.state = {
+    }
+  }
+
+  render () {
+    const {match, location, history} = this.props
+    return (
+      <div className="listings">
+      <div className="container">
+
+    </div>
+    </div>
+  )}
+}
